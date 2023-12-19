948f589ffd558fc15754b814354aa94f21a4896a
Ida Walther <idahwalther@icloud.com>
2023-12-19 13:26:19 +0100

    Klar med G-delen

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
 
+set prisPerPerson = 0;
+set pris = användarinput;
+set antalPersoner = användarinput;
+set dricks = användarinput;
+ 
+function checkNumber ()
+    if (om pris || antalPersoner || dricks inte är ett nummer) 
+        skriv ut “Du måste använda dig av nummer)
+    end if
+    else 
+        kör function calculate() och returnera värdet prisPerPerson;
+        Skriv ut “Varje person ska betala ” + “prisPerPerson” + “ kr”;
+    end else
+end function
 
+function calculate() {
+    let calculatedDricks =  dricks * pris;
+    let summan = calculatedDricks + pris;
+    prisPerPerson = summan / antalPersoner;
+
+    returnera värdet prisPerPerson;
+end function
 */
 
 // VG-Version - Lewis Carroll Word Puzzle
@@ -12,7 +33,7 @@
 function play()
     SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
     SET variabel startOrd till "FOUR";
-    SET variabel slutordOrd till "FIVE";
+    SET variabel slutOrd till "FIVE";
 
 
 end function
