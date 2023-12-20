a4e6c206bab0fd05d399646a2ab5c591e7065973
Ida Walther <idahwalther@icloud.com>
2023-12-20 18:26:43 +0100

    La till VG-delen

--- script.js
+++ script.js
@@ -30,19 +30,50 @@ end function
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
 
-function play()
-    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
-    SET variabel startOrd till "FOUR";
-    SET variabel slutOrd till "FIVE";
+set firstWord = “FOUR”;
+set previousWord = firstWord;
+set lastWord = “FIVE”;
+set keepCount = 0;
+set ordbok = [array med alla existerande engelska ord];
+set loop till true;
 
-
+function play() 
+while(loop)
+    set newWord = inputData;
+    if (newWord INTE är ett ord i ordbok)
+        PRINT “Det där är inget riktigt ord. Försök igen!”
+    end if
+    else if (newWord är ett ord i ordbok)
+        kör funktionen checkWord(previousWord, newWord) och returnera värdet från count.
+        if (värdet från count är lika med noll)
+            PRINT “Du har inte gjort några ändringar! Försök igen”
+        end if
+        else if (värde från count är större än 1)
+                PRINT “Du har gjort fler ändringar än tillåtet. Försök igen!”
+        end else
+        else if (värdet från count är lika med 1)
+            öka värdet på keepCount med 1;
+            kör funktionen checkWord(newWord, lastWord) och returnera count.
+            if (count är 1 eller mer än 1)
+                tilldela previousWord värdet från newWord.
+            end if
+            else if(count är 0) 
+                set loop till false
+            end else
+        end else
+    end else
+end while 
+    PRINT “Grattis! Du lyckades att gå från ” {firstWord} + “ till “ {lastWord} + “ på “ + {keepCount} + “ försök.”
 end function
 
-function isOneLetterApart(wordOne, wordTwo)
-    SET variabel diffCount till 0;
-
-
-    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
-end function
+function checkWord(wordOne, wordTwo);
+    set count till 0;
+    for(varje bokstavsindex i variabeln wordOne)
+        if (wordOne[index] != wordTwo[index]) 
+            count++
+        end if
+    end for
+    returnera count;
+end function;
 
 */
